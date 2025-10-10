f2f0be20d082bbaad40a28f9ee6369e516f6735c
leandrapalmeiras <leandra.pereira.cruz@escola.pr.gov.br>
2025-10-10 07:45:41 -0300

    onkeydown

---
+++ main.js
@@ -0,0 +1,31 @@
+function tocaSomPom(){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play;
+    }
+    else {
+        console.log('Elementonão encontrado ou seletor inválido');
+    }
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listadeTeclas.length; contador ++){
+
+    const tecla = listadeTeclas(contador);
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}';
+
+    tecla.onclick = function (){
+        tocaSomPom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+}
+
+listadeTeclas[0].onclick = tocaSomPom;
